99b0f45528f28e014a63dff8226f1f870326202a
Rafael Castro <ralphc1992@live.com>
2025-05-11 11:20:29 -0500

    updated this

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,8 +8,8 @@ export const Home: React.FC = () => {
     return <>
     <div className="w-full mx-auto max-w-[1400px] mt-20 ">
      <div className="py-20 text-center">
-           <h1 className="offwhite text-3xl">WELCOME TO MMA CHATSHIT</h1>
-        <p className="offwhite-darker">chat shit and have a good time</p>
+           <h1 className="offwhite text-3xl">WELCOME TO MY MMA site</h1>
+        <p className="offwhite-darker">Here are the fights for the upcoming event. Each fight has a dedicated page (comment section). Be respectful and have fun!</p>
         
      </div>
       <UpcomingEvent />
